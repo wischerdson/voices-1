a50142d5ed606f4e5061772238a19d858a2d73ac
Осмухин Даниил <wischerdson@gmail.com>
2024-03-07 12:29:04 +0300

    Hide/show gradient on scroll

--- client/components/Footer.vue
+++ client/components/Footer.vue
@@ -1,6 +1,8 @@
 <template>
 	<div class="fixed bottom-0 inset-x-0 z-20 bg-black">
-		<div class="absolute bottom-full inset-x-0 h-20 bg-gradient-to-t from-black to-transparent z-10 pointer-events-none"></div>
+		<transition>
+			<div class="gradient absolute bottom-full inset-x-0 h-20 bg-gradient-to-t from-black to-transparent z-10 pointer-events-none" v-if="!arrivedState.bottom"></div>
+		</transition>
 		<div class="py-6 max-w-3xl mx-auto">
 			<TextArea
 				class="w-full resize-none max-h-72 bg-gray-900 px-5 py-2.5 leading-normal h-[46px] rounded-lg"
@@ -14,5 +16,27 @@
 <script setup lang="ts">
 
 import TextArea from '~/components/TextArea.vue'
+import { onMounted, ref } from 'vue'
+import { useScroll } from '@vueuse/core'
+
+const $document = ref<Document>()
+
+const { arrivedState } = useScroll($document)
+
+onMounted(() => $document.value = document)
 
 </script>
+
+<style scoped lang="scss">
+
+.gradient {
+	&.v-enter-active, &.v-leave-active {
+		transition: opacity .15s ease;
+	}
+
+	&.v-enter-from, &.v-leave-to {
+		opacity: 0;
+	}
+}
+
+</style>
--- client/components/Header.vue
+++ client/components/Header.vue
@@ -1,5 +1,5 @@
 <template>
-	<div class="fixed top-0 inset-x-0 z-20 bg-black">
+	<div class="header fixed top-0 inset-x-0 z-20 bg-black">
 		<div class="py-4 flex justify-between max-w-3xl mx-auto">
 			<TheLogo />
 			<div class="h-full flex items-center">
@@ -10,12 +10,36 @@
 				</div>
 			</div>
 		</div>
-		<div class="absolute top-full inset-x-0 h-20 bg-gradient-to-b from-black to-transparent z-10 pointer-events-none"></div>
+		<transition>
+			<div class="gradient absolute top-full inset-x-0 h-20 bg-gradient-to-b from-black to-transparent z-10 pointer-events-none" v-if="!arrivedState.top"></div>
+		</transition>
 	</div>
 </template>
 
 <script setup lang="ts">
 
 import TheLogo from '~/components/Logo.vue'
+import { onMounted, ref } from 'vue'
+import { useScroll } from '@vueuse/core'
+
+const $document = ref<Document>()
+
+const { arrivedState } = useScroll($document)
+
+onMounted(() => $document.value = document)
 
 </script>
+
+<style scoped lang="scss">
+
+.gradient {
+	&.v-enter-active, &.v-leave-active {
+		transition: opacity .15s ease;
+	}
+
+	&.v-enter-from, &.v-leave-to {
+		opacity: 0;
+	}
+}
+
+</style>
