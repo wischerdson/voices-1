f28f590c589b2c25c30281eee816ff277de82a88
Осмухин Даниил <wischerdson@gmail.com>
2024-04-10 21:23:40 +0300

    To down btn

--- client/components/MessagesSpace.vue
+++ client/components/MessagesSpace.vue
@@ -4,6 +4,14 @@
 			<MessageList />
 		</div>
 
+		<transition>
+			<div class="to-down-btn fixed bottom-20 z-10 inset-x-0 max-w-3xl mx-auto px-4 pointer-events-none flex justify-end pr-6 pb-6" v-if="infiniteZoneStore.scrollBottom > 200">
+				<TheClickable class="w-7 h-7 bg-gray-300 text-black rounded-full flex items-center justify-center pointer-events-auto" @click="infiniteZoneStore.scrollDown(true)">
+					<icon name="material-symbols:arrow-downward-rounded" />
+				</TheClickable>
+			</div>
+		</transition>
+
 		<WritingStatus />
 	</div>
 </template>
@@ -12,6 +20,7 @@
 
 import MessageList from '~/components/MessageList.vue'
 import WritingStatus from '~/components/WritingStatus.vue'
+import TheClickable from '~/components/Clickable.vue'
 import { onMounted, ref, watch } from 'vue'
 import { useMessagesStore } from '~/store/messages'
 import { useInfiniteZoneStore } from '~/store/infinite-zone'
@@ -55,3 +64,18 @@ onMounted(() => {
 })
 
 </script>
+
+<style scoped lang="scss">
+
+.to-down-btn {
+	&.v-enter-from, &.v-leave-to {
+		opacity: 0;
+		transform: translateY(20px);
+	}
+
+	&.v-enter-active, &.v-leave-active {
+		transition: opacity .25s ease, transform .25s ease;
+	}
+}
+
+</style>
--- client/pages/index.vue
+++ client/pages/index.vue
@@ -8,23 +8,6 @@
 				<MessagesSpace v-else />
 			</div>
 
-			<!-- <transition>
-				<div class="gradient top absolute top-0 inset-x-0 h-20 bg-gradient-to-b from-black to-transparent z-10 pointer-events-none" v-if="!infiniteZoneStore.arrivedTop"></div>
-			</transition>
-			<transition>
-				<div class="gradient bottom absolute bottom-0 inset-x-0 h-20 bg-gradient-to-t from-black to-transparent z-10 pointer-events-none" v-if="!infiniteZoneStore.arrivedBottom"></div>
-			</transition>
-			<InfiniteScrollZone @loadMore="loadMore" v-if="!showInfiniteScrollZone">
-				<MessagesSpace />
-			</InfiniteScrollZone>
-
-			<transition>
-				<div class="to-down-btn absolute bottom-0 z-10 inset-x-0 max-w-3xl mx-auto px-4 pointer-events-none flex justify-end pr-6 pb-6" v-if="infiniteZoneStore.scrollBottom > 200">
-					<TheClickable class="w-7 h-7 bg-gray-300 text-black rounded-full flex items-center justify-center pointer-events-auto" @click="infiniteZoneStore.scrollDown(true)">
-						<icon name="material-symbols:arrow-downward-rounded" />
-					</TheClickable>
-				</div>
-			</transition> -->
 		</div>
 		<div class="fixed inset-x-0 bottom-0 z-30">
 			<WritingArea />
@@ -71,15 +54,6 @@ body {
 	}
 }
 
-.to-down-btn {
-	&.v-enter-from, &.v-leave-to {
-		opacity: 0;
-		transform: translateY(20px);
-	}
 
-	&.v-enter-active, &.v-leave-active {
-		transition: opacity .25s ease, transform .25s ease;
-	}
-}
 
 </style>
