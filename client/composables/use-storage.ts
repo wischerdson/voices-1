5a100d496ce603f12df1317c3bd443be52ce57d5
Осмухин Даниил <wischerdson@gmail.com>
2024-05-22 21:14:24 +0300

    Refactoring client storages

--- client/composables/use-storage.ts
+++ client/composables/use-storage.ts
@@ -1,95 +1,29 @@
+import type { CookieOptions } from 'nuxt/app'
 import type { WatchOptions } from 'vue'
-import { useState } from '#app'
-import { snakeCase } from 'lodash-es'
-import { watch } from 'vue'
-import { singletonClientOnly } from '~/utils/singleton'
-import ClientCookies from 'js-cookie'
+import { defineStorage, cookieStorageDriver, localStorageDriver, dummyStorageDriver } from '~/utils/storages'
 
-export interface StorageDriver<T> {
-	read(): T | null
-	write(data: T | null): void
+type _CookieOptions = CookieOptions & { readonly?: false | undefined; }
+type Storage<T> = ReturnType<typeof defineStorage<T>>
+
+export function useCookieStorage<T>(key: string, init: () => T, cookieOptions?: _CookieOptions, watchOptions?: WatchOptions): Storage<T>
+export function useCookieStorage<T>(key: string, init?: () => T, cookieOptions?: _CookieOptions, watchOptions?: WatchOptions): Storage<T | null>
+export function useCookieStorage<T>(
+	key: string,
+	init?: () => T,
+	cookieOptions: _CookieOptions = {},
+	watchOptions: WatchOptions = {}
+) {
+	return defineStorage(key, cookieStorageDriver(init, cookieOptions), watchOptions)
 }
 
-const handleFetchedData = <T>(rawData: string | null | undefined, init: () => T): T => {
-	if (!rawData) {
-		return (init || (() => null))()
-	}
+export function useLocalStorage<T>(key: string, init: () => T, watchOptions?: WatchOptions): Storage<T>
+export function useLocalStorage<T>(key: string, init?: () => T, watchOptions?: WatchOptions): Storage<T | null>
+export function useLocalStorage<T>(
+	key: string,
+	init?: () => T,
+	watchOptions: WatchOptions = {}
+) {
+	const driver = process.server ? dummyStorageDriver(init) : localStorageDriver(init)
 
-	try {
-		return JSON.parse(rawData);
-	} catch (e) {
-		return rawData as T
-	}
-}
-
-const localStorageDriver = <T>(key: string, init?: () => T): StorageDriver<T> => {
-	if (process.server) {
-		throw new Error('There is impossible to use localStorage driver on the server side')
-	}
-
-	return {
-		read() {
-			return handleFetchedData(
-				window.localStorage.getItem(key),
-				init || (() => null)
-			)
-		},
-		write(data: T) {
-			window.localStorage.setItem(
-				key,
-				typeof data === 'object' ? JSON.stringify(data) : `${data}`
-			)
-		}
-	}
-}
-
-const dummyStorageDriver = <T>(key: string, init?: () => T): StorageDriver<T> => {
-	return {
-		read: init || (() => null),
-		write: () => void 0
-	}
-}
-
-const clientCookieStorageDriver = <T>(key: string, init?: () => T, cookieOptions?: typeof ClientCookies.attributes): StorageDriver<T> => {
-	return {
-		read() {
-			return handleFetchedData(
-				ClientCookies.get(key),
-				init || (() => null)
-			)
-		},
-		write(data: T) {
-			ClientCookies.set(key, JSON.stringify(data), cookieOptions)
-		}
-	}
-}
-
-const useStorage = <T>(stateKey: string, { read, write }: StorageDriver<T>, watchOptions: WatchOptions) => {
-	const state = useState<T | null>(stateKey, read)
-
-	watch(state, write, watchOptions)
-
-	return state
-}
-
-export const useLocalStorage = <T>(key: string, init?: () => T, watchOptions: WatchOptions = {}) => {
-	const stateKey = 'local_storage_' + snakeCase(key)
-
-	return singletonClientOnly(stateKey, () => {
-		const storageKey = 'app_' + snakeCase(key)
-		const driver = process.server ? dummyStorageDriver(storageKey, init) : localStorageDriver(storageKey, init)
-
-		return useStorage<T>(stateKey, driver, watchOptions)
-	})
-}
-
-export const useCookieStorage = <T>(key: string, init?: () => T, watchOptions: WatchOptions = {}) => {
-	const stateKey = 'cookie_' + snakeCase(key)
-
-	return singletonClientOnly(stateKey, () => {
-		const storageKey = 'app_' + snakeCase(key)
-		const driver = process.server ? dummyStorageDriver(storageKey, init) : clientCookieStorageDriver(storageKey, init)
-
-		return useStorage<T>(stateKey, driver, watchOptions)
-	})
+	return defineStorage(key, driver, watchOptions)
 }
---
+++ client/utils/storages.ts
@@ -0,0 +1,102 @@
+import type { CookieOptions } from 'nuxt/app'
+import type { WatchOptions } from 'vue'
+import { watch } from 'vue'
+import { defaultsDeep, isObject, snakeCase } from 'lodash-es'
+import { useCookie, useState } from 'nuxt/app'
+import { singletonClientOnly } from './singleton'
+
+export interface StorageDriver<T> {
+	uid: string
+	read(key: string): T
+	write(key: string, value: T): void
+}
+
+const stringify = (data: unknown): string => typeof data === 'object' ? JSON.stringify(data) : `${data}`
+
+const handleFetchedData = <T>(rawData: string | null | undefined, init?: () => T): T | null => {
+	if (!rawData) {
+		return (init || (() => null))()
+	}
+
+	try {
+		return JSON.parse(rawData) as T;
+	} catch (e) {
+		return rawData as T
+	}
+}
+
+export function cookieStorageDriver<T>(init: () => T, config?: CookieOptions & { readonly?: false | undefined; }): StorageDriver<T>
+export function cookieStorageDriver<T>(init?: () => T, config?: CookieOptions & { readonly?: false | undefined; }): StorageDriver<T | null>
+export function cookieStorageDriver<T>(
+	init?: () => T,
+	config: CookieOptions & { readonly?: false | undefined; } = {}
+): StorageDriver<T | null> {
+	const opts = defaultsDeep(config, {
+		sameSite: 'strict',
+		maxAge: 2147483647
+	})
+
+	return {
+		uid: 'cookie',
+		read(key: string) {
+			return handleFetchedData(
+				useCookie(key, opts).value,
+				init
+			)
+		},
+		write(key: string, value: T) {
+			useCookie(key, opts).value = stringify(value)
+		}
+	}
+}
+
+export function localStorageDriver<T>(init: () => T): StorageDriver<T>
+export function localStorageDriver<T>(init?: () => T): StorageDriver<T | null>
+export function localStorageDriver<T>(init?: () => T): StorageDriver<T | null> {
+	if (process.server) {
+		throw new Error('There is impossible to use localStorage driver on the server side')
+	}
+
+	return {
+		uid: 'localstorage',
+		read(key: string) {
+			return handleFetchedData(
+				window.localStorage.getItem(key),
+				init
+			)
+		},
+		write(key: string, value: T) {
+			window.localStorage.setItem(key, stringify(value))
+		}
+	}
+}
+
+export function dummyStorageDriver<T>(init: () => T): StorageDriver<T>
+export function dummyStorageDriver<T>(init?: () => T): StorageDriver<T | null>
+export function dummyStorageDriver<T>(init?: () => T): StorageDriver<T | null> {
+	return {
+		uid: 'dummy',
+		read: init || (() => null),
+		write: () => void 0
+	}
+}
+
+export const defineStorage = <T>(key: string, driver: StorageDriver<T>, watchOptions: WatchOptions = {}) => {
+	const stateKey = snakeCase(`${driver.uid}_${key}`)
+
+	return singletonClientOnly(stateKey, () => {
+		const storageKey = snakeCase(`app_${key}`)
+
+		const state = useState<T>(stateKey, () => driver.read(storageKey))
+
+		if (isObject(state.value) && !('deep' in watchOptions)) {
+			watchOptions.deep = true
+		}
+
+		const write = (value: T) => driver.write(storageKey, value)
+
+		watch(state, write, watchOptions)
+
+		return { state, write }
+	})
+}
